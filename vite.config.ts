6c5605eef4b4a255f9b20dc27f3f4823832f26c0
Artem <Odintsov.live@ya.ru>
2025-08-04 16:00:49 +0300

    Revert "feat: add user registration page"

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    define: {
-      'import.meta.env.NEXT_PUBLIC_SUPABASE_URL': JSON.stringify(
-        env.NEXT_PUBLIC_SUPABASE_URL,
-      ),
-      'import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY': JSON.stringify(
-        env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-      ),
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
 })
